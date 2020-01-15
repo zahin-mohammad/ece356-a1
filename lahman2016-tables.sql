16a63f7d47dec4a44222e24d3e32e61d6cf42f6d
Zahin Mohammad <zahin.dev@gmail.com>
2020-01-15 10:44:22 -0500

    pitching primary key

--- lahman2016-tables.sql
+++ lahman2016-tables.sql
@@ -514,10 +514,10 @@ CREATE TABLE `ManagersHalf` (
 -- ----------------------------
 DROP TABLE IF EXISTS `Pitching`;
 CREATE TABLE `Pitching` (
-  `playerID` varchar(255) DEFAULT NULL,
-  `yearID` int DEFAULT NULL,
+  `playerID` varchar(255) NOT NULL,
+  `yearID` int NOT NULL,
   `stint` int DEFAULT NULL,
-  `teamID` varchar(255) DEFAULT NULL,
+  `teamID` varchar(255) NOT NULL,
   `lgID` varchar(255) DEFAULT NULL,
   `W` int DEFAULT NULL,
   `L` int DEFAULT NULL,
@@ -544,6 +544,7 @@ CREATE TABLE `Pitching` (
   `SH` varchar(255) DEFAULT NULL,
   `SF` varchar(255) DEFAULT NULL,
   `GIDP` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`playerID`, `yearID`,`teamID`),
   FOREIGN KEY (`playerID`) REFERENCES Master(`playerID`),
   FOREIGN KEY (`teamID`) REFERENCES Teams(`teamID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
