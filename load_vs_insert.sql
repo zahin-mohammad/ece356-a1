ecddafa70014877c64dd2b664cedbb29ba06c78f
Zahin Mohammad <zahin.dev@gmail.com>
2020-01-18 17:48:32 -0500

    q2

---
+++ load_vs_insert.sql
@@ -0,0 +1,25 @@
+
+-- Load the Tables and the Data
+SELECT 'Loading Tables and Data' AS '';
+source /home/z5mohamm/ece356-a1/table_and_data.sql;
+SET FOREIGN_KEY_CHECKS=0;
+
+SELECT 'Starting INSERT test' AS '';
+
+SELECT CURRENT_TIMESTAMP;
+TRUNCATE Batting;
+source /home/z5mohamm/ece356-a1/lahman2016-batting.sql;
+SELECT CURRENT_TIMESTAMP;
+
+SELECT 'Starting LOAD test' AS '';
+TRUNCATE Batting;
+
+SELECT CURRENT_TIMESTAMP;
+LOAD DATA INFILE '/var/lib/mysql-files/Batting.csv' 
+INTO TABLE Batting 
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+SELECT CURRENT_TIMESTAMP;
+
+SET FOREIGN_KEY_CHECKS=1;
